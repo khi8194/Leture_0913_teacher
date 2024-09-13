9e1e6b56720fde919bc16d856ac52d2c8f37a80c
tubi55 <tubi55@nate.com>
2024-09-13 13:29:20 +0900

    es6의 template literal을 활용한 문자열에 변수 삽입 방법

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ const instance = new Student1({ name: 'David', age: 20 });
 //복사가된 각 개별적인 인스턴스들은 자유롭게 prototype에 접근해서 등록된 메서드를 호출가능
 instance.inform();
 
-//ES6에서부터는 위의 불편한 프로토타입 기반 객체지향 분법을 마치 JAVA의 클래스문법을 따라해서 코드 가독성 높임
+//ES6에서부터는 위의 불편한 프로토타입 기반 객체지향 문법을 마치 JAVA의 클래스문법을 따라해서 코드 가독성 높임
 class Student2 {
 	constructor(props) {
 		this.name = props.name;
@@ -21,7 +21,9 @@ class Student2 {
 	}
 
 	inform() {
-		console.log('My Name is ' + this.name + '! ' + 'I am ' + this.age + ' years old.');
+		//template literal '문자열 안에 변수를 삽입하기 위한 ES6전용의 템플릿 문법'
+		//console.log('My Name is ' + this.name + '! ' + 'I am ' + this.age + ' years old.');
+		console.log(`My Name is ${this.name}. and I am ${this.age} years old.`);
 	}
 }
 
